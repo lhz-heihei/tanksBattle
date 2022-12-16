ef7ebbe567bd97054d953ca72c4132b4f33c24fb
lhz <2433924799@qq.com>
2022-12-16 12:11:55 +0800

    camera's size changes in real time

--- Assets/all/scripts/camera_control.cs
+++ Assets/all/scripts/camera_control.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class camera_control : MonoBehaviour
 {
-    public GameObject[] tanks;
-    public Vector3 targetCameraPos = Vector3.zero;
+    public GameObject[] tanks;  //坦克的赋值在game_control里面，因为此脚本的start运行时坦克还未生成
+    private Vector3 targetCameraPos = Vector3.zero;
   
     public Camera mainCamera;
     private Vector3 currentVelocity = Vector3.zero;
@@ -14,13 +14,14 @@ public class camera_control : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;
+        mainCamera = Camera.main;       //主相机
     }
 
     // Update is called once per frame
     void Update()
     {
         CameraPosReset();
+        CameraSizeReset();
     }
 
     void CameraPosReset()
@@ -36,5 +37,18 @@ public class camera_control : MonoBehaviour
             targetCameraPos.y = mainCamera.transform.position.y;
             mainCamera.transform.position = Vector3.SmoothDamp(mainCamera.transform.position, targetCameraPos, ref currentVelocity, smoothTime);
         }
+    }
+
+    void CameraSizeReset()
+    {
+        float size = 0;
+        foreach(var tank in tanks)
+        {
+            Vector3 PosDifference = targetCameraPos - tank.transform.position;
+            size = Mathf.Max(size, Mathf.Abs(PosDifference.z)); //相机的Size是屏幕高度的一半
+            size = Mathf.Max(size, Mathf.Abs(PosDifference.x) / mainCamera.aspect);//mainCamera.aspect : width/height
+        }
+        size += 4;
+        mainCamera.orthographicSize = size;
     }
 }
